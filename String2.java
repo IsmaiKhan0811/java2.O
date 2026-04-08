26699ce6949259d687b6a87d7ff5ef5262b2944a
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-09 01:47:19 +0530

    String different ways to compare

---
+++ String2.java
@@ -0,0 +1,12 @@
+public class String2 {
+    public static void main(String[] args ){
+        String s1 ="Bike";
+        String s2 = new String("Bike");
+        String s3 = new String("Bike");
+        System.out.println(s1==s2);
+        System.out.println(s1==s3);
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));
+    }
+    
+}
