5431c8f23100a92ebf683a03473af71e9c49315c
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-11 01:51:40 +0530

    Mutable String

---
+++ MutableString.java
@@ -0,0 +1,8 @@
+public class MutableString { // Mtable String
+    public static void main(String[] args){
+        StringBuilder str = new StringBuilder("Java"); // way to initialize
+        str.append("with Dsa");
+        System.out.println(str);
+    }
+    
+}
