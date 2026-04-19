0d6cab719c5186cd77bf38c4762b5ea9bb26148d
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-20 03:03:31 +0530

    Java 8 features

--- java8fe.java
+++ java8fe.java
@@ -1,4 +1,4 @@
-interface A{
+interface A{ // Java 8 we write method body in interface
     void show();
     default void config(){ // Intoduce in java 8 feature
         System.out.println("In Config");
