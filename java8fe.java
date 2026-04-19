d303ffc664d5ddc398e83d8dd0b37309416751d0
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-20 03:02:48 +0530

    Java8 Features

---
+++ java8fe.java
@@ -0,0 +1,24 @@
+interface A{
+    void show();
+    default void config(){ // Intoduce in java 8 feature
+        System.out.println("In Config");
+    }
+    static void abc(){// Intoduce in java 8 feature
+        System.out.println("In abc");
+    }
+}
+class B implements A{ // we have to declare the method show() with the help of class
+    public void show(){
+        System.out.println("In show");
+    }
+}
+public class java8fe {
+    public static void main(String[] args ){
+        A.abc(); // Non static method can call directly
+        B obj = new B();
+        obj.show();
+        obj.config();
+
+    }
+    
+}
