a8cc138e8aad622b7426b98df0a51092cb0facf5
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-20 02:13:45 +0530

    Interface Basic

---
+++ Interface.java
@@ -0,0 +1,11 @@
+interface A{
+    int num =6; // Should be public static final
+    void show(); // Methods are publc and abstract
+}
+public class Interface {
+    public static void main(String[] args ){
+        System.out.println(A.num);
+
+    }
+    
+}
