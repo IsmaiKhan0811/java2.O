d8ccf1b33b079e5f2a2e840a252d80c295bdb5b5
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-21 10:48:43 +0530

    Tyoes of Exception

---
+++ Exception.java
@@ -0,0 +1,14 @@
+public class Exception { // Types of execution
+    public static void main(String[] args ){
+        int a =8;
+        //  1 ibt b =2; Syntax error
+        int b= 0;
+        // 2 int result = a-b;       (Logical Error)
+        // 2 System.out.println("Multiply is : " + result);
+        // System.out.println("Divison is : " + a/b); // 3 Runtime Error Execution stop
+        
+        System.out.println("Byeee"); // This line nvr run but this writtem in vell manner 
+
+    }
+    
+}
